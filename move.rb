c482e97978df0fdd7851fc346a63ca9bb9b44bde
William <william.l.munoz@gmail.com>
2020-04-13 17:03:48 +0000

    Automatically backed up by Learn

--- move.rb
+++ move.rb
@@ -6,4 +6,12 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
+ def input_to_index(input)
+   input = index.to_i - 1
+end
+
+def move(board, index, user_marker = "X")
+  board[index.to_i]
+
+
 # code your input_to_index and move method here!
